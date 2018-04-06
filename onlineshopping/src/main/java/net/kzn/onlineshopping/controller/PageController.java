ad0905582f6f5ad79d55c9cdbdf5d1a1939785ee
abhishek7731 <37328551+abhishek7731@users.noreply.github.com>
2018-04-06 17:14:56 +0530

    COMPLETED THE INTERFACE THEME AND OTHER ISSUES

--- onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -21,12 +21,46 @@ public class PageController {
 													// giving page and actual
 													// page is residing into
 													// views-->page.jsp
-		mv.addObject("greeting", "welcome to spring web mvc"); // here greeting
-																// is the view
-																// and it will
-																// be passed to
-																// page.jsp
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
 		return mv;
 	}
 
+	@RequestMapping(value = "/contact") // home ,index these are
+	// request and who will
+	// manage these request
+	// it will be managed by
+	// model and view neecha
+	// dekho
+	public ModelAndView contact() {
+		ModelAndView mv = new ModelAndView("page"); // here view name is page it
+		// is not the logical name
+		// as the logical name is
+		// page .jsp but we are
+		// giving page and actual
+		// page is residing into
+		// views-->page.jsp
+		mv.addObject("title", "Contact Us"); //here title everywhere will be passed to page.jsp
+		mv.addObject("userClickContact", true);
+		return mv;
+	}
+
+	@RequestMapping(value = "/about") // home ,index these are
+	// request and who will
+	// manage these request
+	// it will be managed by
+	// model and view neecha
+	// dekho
+	public ModelAndView about() {
+		ModelAndView mv = new ModelAndView("page"); // here view name is page it
+		// is not the logical name
+		// as the logical name is
+		// page .jsp but we are
+		// giving page and actual
+		// page is residing into
+		// views-->page.jsp
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", true);
+		return mv;
+	}
 }
